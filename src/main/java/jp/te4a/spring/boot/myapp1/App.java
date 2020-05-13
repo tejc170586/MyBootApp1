476905cc2e7ab1a73472f3a44f7ad4cab70cb059
tejc170586 <aliendenpa1109@gmail.com>
2020-05-13 23:50:20 +0900

    Exercise_1

--- src/main/java/jp/te4a/spring/boot/myapp1/App.java
+++ src/main/java/jp/te4a/spring/boot/myapp1/App.java
@@ -1,13 +1,22 @@
 package jp.te4a.spring.boot.myapp1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
+ @Configuration
+ @EnableAutoConfiguration
+ @ComponentScan
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
     }
 }
